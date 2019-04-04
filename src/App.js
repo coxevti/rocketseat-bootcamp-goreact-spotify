2cb7b72a3b25a86f63b065b8d23fe85d7b84abb5
Valdir Coxev <seti1@zornimat.com.br>
2019-04-04 11:33:30 -0400

    Aula 2: Estilizando Sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
+import Sidebar from './components/Sidebar';
 
 import GlobalStyle from './styles/global';
 
 const App = () => (
   <Fragment>
     <GlobalStyle />
-    <h1>Hello Word</h1>
+    <Sidebar />
   </Fragment>
 );
 
